1b2bc458a3d0f084bd6c0ed962eb4c99be0a3fab
Jules95Game <romp.julian1@gmail.com>
2023-10-29 15:02:28 +0100

    IRepository interface

---
+++ JavaExerciserAngular.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace JavaExerciserAngular.Domain.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : IEntity
+{
+    public IQueryable<TEntity> GetAll();
+
+    public IQueryable<TEntity> GetByID(int id);
+
+    public TEntity Update(TEntity entity);
+}
