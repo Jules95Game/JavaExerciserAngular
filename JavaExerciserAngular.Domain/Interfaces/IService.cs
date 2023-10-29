e5ae1679f5267b29db3e6637ca9bd1bb4d9f5577
Jules95Game <romp.julian1@gmail.com>
2023-10-29 15:11:34 +0100

    IService interface

---
+++ JavaExerciserAngular.Domain/Interfaces/IService.cs
@@ -0,0 +1,12 @@
+﻿namespace JavaExerciserAngular.Domain.Interfaces;
+
+public interface IService<TEntity> where TEntity : IEntity
+{
+    public List<TEntity> GetAll();
+
+    public List<TEntity> SearchByQuery(string query);
+
+    public TEntity? GetById(int id);
+
+    public TEntity? Update(TEntity entity);
+}
