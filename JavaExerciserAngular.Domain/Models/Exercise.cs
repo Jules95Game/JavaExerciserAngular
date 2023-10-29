82a5189c587e5cffbb5f7e0d0c36d3745cb89086
Jules95Game <romp.julian1@gmail.com>
2023-10-29 14:55:30 +0100

    Exercise model

--- JavaExerciserAngular.Domain/Models/Exercise.cs
+++ JavaExerciserAngular.Domain/Models/Exercise.cs
@@ -1,7 +1,14 @@
-﻿namespace JavaExerciserAngular.Domain.Models
-{
-    public class Exercise
-    {
+﻿using JavaExerciserAngular.Domain.Interfaces;
 
-    }
+namespace JavaExerciserAngular.Domain.Models;
+
+public class Exercise : IEntity
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required int Size { get; set; }
+    public required List<string> Techniques { get; set; }
+    public required string Instructions { get; set; }
+    public Status Status { get; set; }
+    public DateTime StatusTime { get; set; }
 }
