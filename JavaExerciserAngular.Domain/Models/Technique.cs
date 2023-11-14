7ac269becb08641d610ad9b1d620bb2ca3c6e0ce
Jules95Game <romp.julian1@gmail.com>
2023-11-14 12:42:49 +0100

    Added Technique class

---
+++ JavaExerciserAngular.Domain/Models/Technique.cs
@@ -0,0 +1,9 @@
+﻿using JavaExerciserAngular.Domain.Interfaces;
+
+namespace JavaExerciserAngular.Domain.Models;
+
+public class Technique : IEntity
+{
+    public int Id { get; set; }
+    public required string TechniqueName { get; set; }
+}
